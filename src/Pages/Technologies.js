747f6214cc0ecda8589853e08a27e2b6327d3d05
Ryfria <souoryria2011@gmail.com>
2026-08-26 16:38:26 +0700

    Update Technologies.js

--- src/Pages/Technologies.js
+++ src/Pages/Technologies.js
@@ -33,18 +33,42 @@ function Technologies() {
         </p>
       </section>
       <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
-        <img src={html} title="html" alt="" />
-        <img src={css} title="CSS" alt="" />
-        <img src={js} title="JavaScript" alt="" />
-        <img src={php} title="PHP" alt="" />
-        <img src={bootstrap} title="Bootstrap" alt="" />
-        <img src={python} title="Python" alt="" />
-        <img src={ruijie} title="Ruijie | Reyee" alt="" />
-        <img src={mikrotik} title="MikroTIK" alt="" />
-        <img src={cisco} title="CISCO" alt="" />
-        <img src={juniper} title="Juniper Networks" alt="" />
-        <img src={huawei} title="HUAWEI" alt="" />
-        <img src={fortinet} title="FORTINET" alt="" />
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={html} title="html" alt="" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={css} title="CSS" alt="" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={js} title="JavaScript" alt="" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={php} title="PHP" alt="" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={bootstrap} title="Bootstrap" alt="" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={python} title="Python" alt="" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={ruijie} title="Ruijie | Reyee" alt="" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={mikrotik} title="MikroTIK" alt="" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={cisco} title="CISCO" alt="" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={juniper} title="Juniper Networks" alt="" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={huawei} title="HUAWEI" alt="" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={fortinet} title="FORTINET" alt="" />
+        </div>
       </section>
       <section>
         <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
@@ -52,12 +76,24 @@ function Technologies() {
         </h1>
       </section>
       <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
-        <img src={vscode} title="Visual Studio Code" alt="" />
-        <img src={git} title="Git" alt="Git" />
-        <img src={github} title="Github" alt="Github" />
-        <img src={putty} title="Putty" alt="Putty" />
-        <img src={npm} title="NPM" alt="NPM" />
-        <img src={winbox} title="Winbox" alt="Winbox" />
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={vscode} title="Visual Studio Code" alt="" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={git} title="Git" alt="Git" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={github} title="Github" alt="Github" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={putty} title="Putty" alt="Putty" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={npm} title="NPM" alt="NPM" />
+        </div>
+        <div className="bg-white rounded-lg p-2 flex items-center justify-center">
+          <img src={winbox} title="Winbox" alt="Winbox" />
+        </div>
       </section>
     </main>
   );
